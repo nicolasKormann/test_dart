393febdcd5384880177d19c80769681ed57fafb0
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-08-16 14:33:47 -0300

    update dart_test

--- bin/dart_test.dart
+++ bin/dart_test.dart
@@ -1,5 +1,5 @@
 import 'package:dart_test/dart_test.dart' as dart_test;
 
 void main(List<String> arguments) {
-  print('O somatório de todos os valores inteiros divisíveis por 3 ou 5 é: ${dart_test.sum()}!');
+  print('O somatório de todos os valores inteiros divisíveis por 3 ou 5 é: ${dart_test.sum()}');
 }
